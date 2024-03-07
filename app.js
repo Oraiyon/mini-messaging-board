984b23d1fc2ad11bb4e7ea4e7172eb70062bf76c
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-07 12:56:13 +0800

    Removed path.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -10,14 +9,14 @@ const app = express();
 app.listen(8080);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
