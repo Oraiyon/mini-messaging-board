e9ff2f734795e324ee929f6f93c28c11c24f9ef4
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-07 12:53:35 +0800

    Added routers to files in /views.

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,34 @@
+const messages = [
+  {
+    text: "Hello, World!",
+    user: "Oliver",
+    added: new Date()
+  },
+  {
+    text: "Woof",
+    user: "Thor",
+    added: new Date()
+  }
+];
 
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Mini Messaging Board", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("new");
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  });
+  res.redirect("/");
+});
+
+module.exports = router;
